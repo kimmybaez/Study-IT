a5c52f03f1ca5daabd47044af44d00cb2bc28196
Кирилл <kimmybaez@vk.com>
2022-08-08 16:33:31 +0300

    header

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Study-IT > Обучай и обучайся</title>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <style>
+        .header__logo {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <?php require("components/header.html"); ?>
+    </div>
+</body>
+</html>
